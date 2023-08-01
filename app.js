22853bf1f2958e1c6e7991a0c9928c21a32dde76
giles lewey <gileslewey@giless-MacBook-Pro.local>
2023-07-31 21:33:20 -0500

    port

--- app.js
+++ app.js
@@ -64,8 +64,16 @@ app.use((err, req, res, next) => {
     }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function () {
-    // ...
-  });
+
+const PORT = process.env.PORT || 3000
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`Hello ${process.env.HELLO}`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}/`)
+})
 
